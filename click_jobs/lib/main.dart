4b3d9d644d2ad8d63364200a4d332b42c81dc829
anselcodes <elfaael12@gmail.com>
2021-05-05 23:53:48 +0800

    add routes

--- click_jobs/lib/main.dart
+++ click_jobs/lib/main.dart
@@ -1,4 +1,10 @@
+import 'package:click_jobs/pages/home_page.dart';
+import 'package:click_jobs/pages/onboarding_screen.dart';
+import 'package:click_jobs/pages/sign_in_page.dart';
+import 'package:click_jobs/pages/sign_up_page.dart';
+import 'package:click_jobs/pages/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +15,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
       routes: {
-        //    '/': (context) => SplashPage(),
-        //    '/onboarding': (context) => OnboardingPage(),
-        //    '/sign-in': (context) => SignInPage(),
-        //    '/sign-up': (context) => SignUpPage(),
-        //    '/home': (context) => HomePage(),
+        '/': (context) => SplashPage(),
+        '/onboarding': (context) => OnboardingPage(),
+        '/sign-in': (context) => SignInPage(),
+        '/sign-up': (context) => SignUpPage(),
+        '/home': (context) => HomePage(),
       },
     );
   }
