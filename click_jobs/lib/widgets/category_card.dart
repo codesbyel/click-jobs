bfd601ee67fc71b736808bad4af52e7bd1057f28
anselcodes <elfaael12@gmail.com>
2021-05-05 23:55:03 +0800

    add widget category

---
+++ click_jobs/lib/widgets/category_card.dart
@@ -0,0 +1,51 @@
+import 'package:click_jobs/pages/category_page.dart';
+import 'package:flutter/material.dart';
+
+import '../theme.dart';
+
+class CategoryCard extends StatelessWidget {
+  final String name;
+  final String imageUrl;
+
+  CategoryCard({this.imageUrl, this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => CategoryPage(
+              name: name,
+              imageUrl: imageUrl,
+            ),
+          ),
+        );
+      },
+      child: Container(
+        width: 150,
+        height: 200,
+        margin: EdgeInsets.only(right: defaultMargin),
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              imageUrl,
+            ),
+          ),
+        ),
+        child: Align(
+          alignment: Alignment.bottomLeft,
+          child: Text(
+            name,
+            style: whiteTextStyle.copyWith(
+              fontSize: 18,
+              fontWeight: medium,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
