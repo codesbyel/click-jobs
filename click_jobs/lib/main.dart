3064ff89ed5751f15cba8d4bfba7baacffa89196
anselcodes <elfaael12@gmail.com>
2021-05-07 12:30:00 +0800

    added provider for category, user model, auth provider

--- click_jobs/lib/main.dart
+++ click_jobs/lib/main.dart
@@ -3,8 +3,12 @@ import 'package:click_jobs/pages/onboarding_screen.dart';
 import 'package:click_jobs/pages/sign_in_page.dart';
 import 'package:click_jobs/pages/sign_up_page.dart';
 import 'package:click_jobs/pages/splash_page.dart';
+import 'package:click_jobs/providers/auth_provider.dart';
+import 'package:click_jobs/providers/category_provider.dart';
+import 'package:click_jobs/providers/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,18 +17,31 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthProvider>(
+          create: (context) => AuthProvider(),
+        ),
+        ChangeNotifierProvider<UserProvider>(
+          create: (context) => UserProvider(),
+        ),
+        ChangeNotifierProvider<CategoryProvider>(
+          create: (context) => CategoryProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(),
+        ),
+        routes: {
+          '/': (context) => SplashPage(),
+          '/onboarding': (context) => OnboardingPage(),
+          '/sign-in': (context) => SignInPage(),
+          '/sign-up': (context) => SignUpPage(),
+          '/home': (context) => HomePage(),
+        },
       ),
-      routes: {
-        '/': (context) => SplashPage(),
-        '/onboarding': (context) => OnboardingPage(),
-        '/sign-in': (context) => SignInPage(),
-        '/sign-up': (context) => SignUpPage(),
-        '/home': (context) => HomePage(),
-      },
     );
   }
 }
